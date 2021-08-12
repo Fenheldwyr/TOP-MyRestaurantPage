66f0c40e87af25a1c1e84e882d02d9068dbb7c46
Fenheldwyr <18633743+Fenheldwyr@users.noreply.github.com>
2021-08-12 12:23:19 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+    },
+    mode: "development",
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "My Restaurant",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
